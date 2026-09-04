47de0760feeb26d6d3f13bc976efc8062c97f510
Kohxax <koharen0256@gmail.com>
2026-09-05 07:13:41 +0900

    enhance: Nuxt UIを導入 (#1)

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     databaseUrl: process.env.DATABASE_URL ?? './data/storage.db',
